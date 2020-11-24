5fdfb3a9ad3b7aceb6cea29015e16a6f05062454
arianezequiel01 <arbustamante@alumno.unlam.edu.ar>
2020-11-24 20:37:40 -0300

    t

--- src/ar/edu/unlam/pb220202c/eva03/Automovil.java
+++ src/ar/edu/unlam/pb220202c/eva03/Automovil.java
@@ -1,6 +1,55 @@
 package ar.edu.unlam.pb220202c.eva03;
 
-public class Automovil {
+public class Automovil extends Vehiculo {
+	
+	protected Integer cantidadAsientos;
+	protected String tipo;
+	protected static Integer limiteVelocidadAutomovil=130;
+	
+	
+
+	public Automovil(String patente, Integer velocidadActual, Integer limiteVelocidad, Integer cantidadAsientos, String tipo) {
+		super(patente, velocidadActual, limiteVelocidad);
+		this.cantidadAsientos=cantidadAsientos;
+		this.tipo=tipo;
+		limiteVelocidad=limiteVelocidadAutomovil;
+	}
+
+
+
+	public Integer getCantidadAsientos() {
+		return cantidadAsientos;
+	}
+
+
+
+	public void setCantidadAsientos(Integer cantidadAsientos) {
+		this.cantidadAsientos = cantidadAsientos;
+	}
+
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+	public static Integer getLimiteVelocidadAutomovil() {
+		return limiteVelocidadAutomovil;
+	}
+
+
+
+	public static void setLimiteVelocidadAutomovil(Integer limiteVelocidadAutomovil) {
+		Automovil.limiteVelocidadAutomovil = limiteVelocidadAutomovil;
+	}
 
 	//Si es necesario Utilice herencia o implemente de Interfaces
 //	Se debe crear contructeres getters y Setters y loos que crean convenientes
--- src/ar/edu/unlam/pb220202c/eva03/Vehiculo.java
+++ src/ar/edu/unlam/pb220202c/eva03/Vehiculo.java
@@ -2,13 +2,61 @@ package ar.edu.unlam.pb220202c.eva03;
 
 public class Vehiculo {
 
-	//Se debe crear contructeres getters y Setters y loos que crean convenientes
-	public String Patente;
-	public Integer VelocidadActual;
-	public Integer limiteVelocidad;
+	private String Patente;
+	private Integer VelocidadActual;
+	private Integer limiteVelocidad;
 
 	
-	public void incrmentarVelocidad(Integer Velocidad) {
-		
+	public void incrmentarVelocidad(Integer Velocidad) 
+	{
+		VelocidadActual=Velocidad;
 	}
+
+	public Vehiculo(String patente, Integer velocidadActual, Integer limiteVelocidad) 
+	{
+		super();
+		Patente = patente;
+		VelocidadActual = velocidadActual;
+		this.limiteVelocidad = limiteVelocidad;
+	}
+
+
+	public String getPatente() 
+	{
+		return Patente;
+	}
+
+
+	public void setPatente(String patente)
+	{
+		Patente = patente;
+	}
+
+
+	public Integer getVelocidadActual() 
+	{
+		return VelocidadActual;
+	}
+
+
+	public void setVelocidadActual(Integer velocidadActual) 
+	{
+		VelocidadActual = velocidadActual;
+	}
+
+
+	public Integer getLimiteVelocidad() 
+	{
+		return limiteVelocidad;
+	}
+
+
+	public void setLimiteVelocidad(Integer limiteVelocidad) 
+	{
+		this.limiteVelocidad = limiteVelocidad;
+	}
+	
+	
+	
+	
 }
